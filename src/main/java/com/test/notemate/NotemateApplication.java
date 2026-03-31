7446366cd750278d66fe80ff575f72f1ca62d9d4
ppjh94 <wleer157@gmail.com>
2026-03-31 22:45:45 +0900

    init: Spring Boot 프로젝트 설정

---
+++ src/main/java/com/test/notemate/NotemateApplication.java
@@ -0,0 +1,13 @@
+package com.test.notemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/notemate/NotemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.notemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
