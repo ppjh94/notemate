60a4388939adbe21a2adbdf80dfe1ee89f58e35f
ppjh94 <wleer157@gmail.com>
2026-04-06 23:56:49 +0900

    feat: User, Content Repository 추가

---
+++ src/main/java/com/test/notemate/domain/content/repository/ContentRepository.java
@@ -0,0 +1,18 @@
+package com.test.notemate.domain.content.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.test.notemate.domain.content.entity.Content;
+
+public interface ContentRepository extends JpaRepository<Content, Long> {
+
+	/* 전체 콘텐츠 조회 */
+	List<Content> findAllByOrderByIdDesc();
+	
+	/* 무료 콘텐츠만 조회 */
+	List<Content> fintByPremiumFalseOrderByIdDesc();
+	
+	/* 프리미엄 콘텐츠만 조회 */
+	List<Content> findByPremiumTrueOrderByIdDesc();
+	
+}
