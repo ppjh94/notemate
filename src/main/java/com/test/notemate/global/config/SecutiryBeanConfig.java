641487c65d47cc629aa46b36dd3be6155508272a
ppjh94 <wleer157@gmail.com>
2026-04-14 00:24:27 +0900

    refactor: Spring Security 임시 비활성화 조치

--- src/main/java/com/test/notemate/global/config/SecutiryBeanConfig.java
+++ src/main/java/com/test/notemate/global/config/SecutiryBeanConfig.java
@@ -5,8 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/*Srping Security 설정 비활성화*/
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 @Configuration
 public class SecutiryBeanConfig {
+
+	/*Srping Security 설정 임시 비활성화*/
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+		return http.build();
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
